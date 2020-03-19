2c19d4c95bd2b0df2982d0c04a5d3559c3f17b64
juanpajuarez <201503869@ingenieria.usac.edu.gt>
2020-03-19 16:53:31 -0600

    Conexion a SQL Server

---
+++ ConexionBD/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ConexionBD._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <asp:DataGrid ID="DGEmpleados" runat="server"></asp:DataGrid>
+    </div>
+
+</asp:Content>
---
+++ ConexionBD/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConexionBD
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string QueryGetEmpleados = "SELECT empleado.nombre,empleado.sueldo,departamento.nombre FROM dbo.empleado " +
+                    "JOIN dbo.departamento on empleado.idDepartamento = departamento.id_departamento; ";
+                using (SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conexionBD"].ConnectionString))
+                {
+                    SqlCommand comando = new SqlCommand(QueryGetEmpleados, conexion);
+                    conexion.Open();
+                    DGEmpleados.DataSource = comando.ExecuteReader();
+                    DGEmpleados.DataBind();
+                    conexion.Close();
+                }
+            }
+        }
+    }
+}
